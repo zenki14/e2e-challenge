32a2183a14bc6adaa86e73c67a8f004dcc581300
Dane Worsham <zenki14@gmail.com>
2020-10-27 23:39:20 -0500

    added test coverage for suggested search results and search and assert on url test cases

--- test/pageobjects/home.page.js
+++ test/pageobjects/home.page.js
@@ -4,11 +4,26 @@ class FormPage extends Page {
     // Elements go here
     get searchInput () { return $('#searchInput') }
     get searchInputButton () { return $('#searchButton') }
+    get suggestionFirst () { return $$('span.highlight')[0] }
+    get suggestionSecond () { return $$('span.highlight')[1] }
 
     // Methods go here
     setSearchText (text) {
         this.searchInput.setValue(text)
     }
+
+    getFirstSuggestion () {
+        this.suggestionFirst.getText()
+    }
+
+    getSecondSuggestion () {
+        this.suggestionSecond.getText()
+    }
+
+    clickFirstSuggestion () {
+        this.suggestionFirst.click()
+    }
+
     search () {
         this.searchInputButton.click()
     }
--- test/specs/home.spec.js
+++ test/specs/home.spec.js
@@ -10,7 +10,23 @@ describe('Wikipedia home page',  () => {
     })
 
     it('should allow the user to search and display results', () => {
-        HomePage.setSearchText("world's tallest building")
-        browser.pause(10000)
+        const searchText = "world's tallest building"
+        HomePage.setSearchText(searchText)
+        browser.waitUntil(() => $('.suggestions').isDisplayed() === true)
+    })
+
+    it('should show a dropdown of suggested results', () => {
+        const expectedFirstSuggestion = "World's Tallest Building"
+        const expectedSecondSuggestion = "World's tallest buildings"
+
+        HomePage.getFirstSuggestion() === expectedFirstSuggestion
+        HomePage.getSecondSuggestion() === expectedSecondSuggestion
+    })
+
+    it('clicks the first suggested result and verifies new url', () => {
+        const expected = "https://en.wikipedia.org/wiki/List_of_tallest_buildings"
+        HomePage.clickFirstSuggestion()
+        browser.waitUntil(() => $('#firstHeading').getText() === "List of tallest buildings")
+        browser.getUrl() === expected
     })
 })
