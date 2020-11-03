772e387c4df534af456a3117e18aed6b44480036
Dane Worsham <zenki14@gmail.com>
2020-11-02 18:16:10 -0600

    checking in broken stuff

--- test/pageobjects/lotb.page.js
+++ test/pageobjects/lotb.page.js
@@ -3,15 +3,36 @@ import Page from './page'
 class FormPage extends Page {
     // Elements go here
     get sortableTable () { return $('table.wikitable.sortable') }
-    get sortClick () { return $('table.wikitable.sortable > thead > tr > th')[8] }
+    get sortClick () { return $('table.wikitable.sortable.jquery-tablesorter:nth-child(25) > thead:nth-child(1) > tr:nth-child(1) > th.headerSort:nth-child(8)') }
+    get topResult () { return $('table.wikitable.sortable.jquery-tablesorter:nth-child(25) > tbody:nth-child(2) > tr:nth-child(1) > td:nth-child(2)') }
+    get feet () { return $('table.wikitable.sortable.jquery-tablesorter:nth-child(25) > tbody:nth-child(2) > tr:nth-child(1) > td:nth-child(7)') }
+    get meters () { return $('table.wikitable.sortable.jquery-tablesorter:nth-child(25) > tbody:nth-child(2) > tr:nth-child(1) > td:nth-child(6)') }
 
     // Methods go here
     verifyTable () {
         this.sortableTable.isExisting()
     }
 
-    sortList () {
-      this.sortClick.click()
+    getHeightFeet () {
+        this.feet.getText()
+    }
+
+    getHeightMeters () {
+        this.meters.scrollIntoView()
+        console.log(this.meters.getValue())
+        this.meters.getValue()
+    }
+
+    sortListText () {
+        this.sortClick.getText()
+    }
+
+    sortListClick () {
+        this.sortClick.click()
+    }
+
+    firstBuilding () {
+        this.topResult.getText()
     }
 }
 
--- test/specs/lotb.spec.js
+++ test/specs/lotb.spec.js
@@ -1,3 +1,4 @@
+import lotbPage from '../pageobjects/lotb.page'
 import LotbPage from '../pageobjects/lotb.page'
 
 describe('List of Tallest Buildings page', () => {
@@ -5,9 +6,36 @@ describe('List of Tallest Buildings page', () => {
       browser.url('https://en.wikipedia.org/wiki/List_of_tallest_buildings')
       const expected = "https://en.wikipedia.org/wiki/List_of_tallest_buildings"
       browser.getUrl() === expected
+      expect(browser).toHaveUrl(expected)
     })
 
     it('should verify the table of buildings exists', () => {
       LotbPage.verifyTable() === true
+    })
+    it('should verify that the correct building height is displayed based on custom conversion function', () => {
+      const meters = LotbPage.getHeightMeters()
+      const feet = 2073
+      const conversion = Math.round(meters * 3.2808)
+      LotbPage.getHeightFeet() === feet
+      // LotbPage.getHeightMeters() === meters
+
+
+      browser.pause(100000)
+      expect(conversion).toEqual(feet)
+
+
+
+
+    })
+    it('should sort by Year', () => {
+      LotbPage.sortListText() === 'Year'
+      LotbPage.sortListClick()
+    })
+
+    it('verifies top result after sorting', () => {
+      const topResult = LotbPage.firstBuilding()
+      const expected = "Empire State Building"
+      browser.pause(1000)
+      expect(topResult).toEqual(expected)
     })
 })
